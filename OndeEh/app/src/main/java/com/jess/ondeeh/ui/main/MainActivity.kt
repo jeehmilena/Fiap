679cdb4245a7a5b26a59febff76d37a8e666958f
jeehmilena <jessicamilenacorrea@gmail.com>
2021-08-17 20:04:18 -0300

    Projeto estudo de arquiteturas

---
+++ OndeEh/app/src/main/java/com/jess/ondeeh/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jess.ondeeh.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jess.ondeeh.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
